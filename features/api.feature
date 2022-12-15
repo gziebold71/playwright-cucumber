2902f4083671afc9d548874a0024a5f0baa98f93
gziebold71 <110841152+gziebold71@users.noreply.github.com>
2022-12-15 16:52:51 -0700

    change library usage for api call

--- features/api.feature
+++ features/api.feature
@@ -1,7 +1,7 @@
 Feature: Get offsets
 
-  @demo
-  Scenario: verify that a valid GET request to offsets returns a 200 and the correct account and list type for UNLISTED offsets
-    Given make a GET request for "/hem/v1/offsets?account_id=0.0.47879120&list_state=UNLISTED"
+  @demo @nobrowser
+  Scenario: verify that a valid GET request to offsets returns a 200 and the correct limit
+    Given make a GET request for "/api/offsets/unlisted?limit=17"
     Then Verify the response status code is '200'
-    And I verify that all returned offsets are of type "UNLISTED"
+    And I verify that "17" offsets are returned
--- steps/api.steps.ts
+++ steps/api.steps.ts
@@ -3,22 +3,25 @@ import {Given, When, Then} from "@cucumber/cucumber";
 import {expect} from '@playwright/test'
 import {config} from "../support/config";
 
+const request = require('supertest');
+
 let apiResponse;
 
 Given('make a GET request for {string}', async function (this: ICustomWorld, uri: string) {
-    apiResponse = await this.page.request.get(config.exchangeApiUrl + uri, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+    apiResponse = await request(config.exchangeApiUrl).get(uri).set('Accept', 'application/json')
 });
 
 Then('Verify the response status code is {string}', async function (responseCode) {
-    expect(apiResponse.ok()).toBeTruthy();
+    expect(apiResponse.statusCode).toEqual(parseInt(responseCode));
+})
+
+Then('I verify that {string} offsets are returned', async (countOfOffsets) => {
+    const body = await JSON.parse(apiResponse.text)
+    expect(body.offsets.length).toEqual(parseInt(countOfOffsets))
 })
 
 Then('I verify that all returned offsets are of type {string}', async (listState) => {
-    const body = await apiResponse.json()
+    const body = await JSON.parse(apiResponse.text)
     for (let i = 0; i < body.offsets.length; i++) {
         expect(body.offsets[i].list_state).toEqual(listState);
     }
